165218d0e165356583b25994e18783369b646e36
matthewdoles <matteddied@gmail.com>
2019-10-22 11:14:31 -0400

    Exercise 2-9 Contact Selected

--- force-app/Exercises/main/default/lwc/studentTile/studentTile.js
+++ force-app/Exercises/main/default/lwc/studentTile/studentTile.js
@@ -1,16 +1,18 @@
 import { LightningElement, api } from "lwc";
 
 export default class StudentTile extends LightningElement {
+	@api selectedStudentId = '';
 	@api student = {
 		Name: "Matthew Doles",
 		PhotoUrl: "/services/images/photo/003B0FakePictId"
 	};
-	@api selected = false;
 	get tileSelected() {
-		return this.selected ? "tile selected " : "tile";
+		return (this.selectedStudentId===this.student.Id) ? "tile selected" : "tile";
 	}
 	studentClick() {
-		// eslint-disable-next-line no-alert
-		alert(this.student.Name);
+		const evt = new CustomEvent("studentselected", {
+			detail: { studentId: this.student.Id }
+		});
+		this.dispatchEvent(evt);
 	}
 }
