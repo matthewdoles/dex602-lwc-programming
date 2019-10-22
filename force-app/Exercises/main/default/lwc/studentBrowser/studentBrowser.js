9f0f92b2fa47fd7852525aaed1111d6a873609d3
matthewdoles <matteddied@gmail.com>
2019-10-22 13:22:50 -0400

    Exercise 2-10 Completed

--- force-app/Exercises/main/default/lwc/studentBrowser/studentBrowser.js
+++ force-app/Exercises/main/default/lwc/studentBrowser/studentBrowser.js
@@ -1,15 +1,27 @@
-import { LightningElement, track, wire } from 'lwc';
-import getStudents from '@salesforce/apex/StudentBrowser.getStudents';
+import { LightningElement, track, wire } from "lwc";
+import getStudents from "@salesforce/apex/StudentBrowser.getStudents";
+import { fireEvent } from "c/pubsub";
+import { CurrentPageReference } from "lightning/navigation";
 
 export default class StudentBrowser extends LightningElement {
-    @track selectedInstructorId;
-    @track selectedDeliveryId;
-    
-    @wire(getStudents,{ instructorId: "$selectedInstructorId", courseDeliveryId: "$selectedDeliveryId"}) 
-    students;
+	@track selectedInstructorId;
+	@track selectedDeliveryId;
 
-    handleFilterChange(event) {
-        this.selectedInstructorId = event.detail.instructorId;
-        this.selectedDeliveryId = event.detail.deliveryId;
-    }
+	@wire(CurrentPageReference) pageRef;
+
+	@wire(getStudents, { instructorId: "$selectedInstructorId", courseDeliveryId: "$selectedDeliveryId" })
+	students;
+
+	handleFilterChange(event) {
+		this.selectedInstructorId = event.detail.instructorId;
+		this.selectedDeliveryId = event.detail.deliveryId;
+	}
+
+	handleStudentSelected(event) {
+		const studentId = event.detail.studentId;
+		this.updateSelectedStudent(studentId);
+	}
+	updateSelectedStudent(studentId) {
+		fireEvent(this.pageRef, "studentChange", { studentId });
+	}
 }
