e517620f872c60348a1172ae16a2781dae04f979
Matthew Doles <matteddied@gmail.com>
2019-10-23 15:29:25 -0400

    Exercise 4-4 Complete

---
+++ force-app/Exercises/main/default/lwc/challenge_contactDirectory/challenge_contactDirectory.js
@@ -0,0 +1,43 @@
+import { LightningElement, track, wire } from "lwc";
+import getContacts from "@salesforce/apex/Contacts.getContacts";
+
+export default class Challenge_contactDirectory extends LightningElement {
+    @track contacts;
+    @track firstLetter;
+    error;
+
+    @wire(getContacts, { firstLetter: "$firstLetter" })
+    wired_getContacts(result) {
+		this.contacts = [];
+		if (result.data) {
+			result.data.forEach(contact => {
+				this.contacts.push({
+					contactId: contact.Id,
+					name: contact.Name,
+					email: contact.Email,
+					phone: contact.Phone
+				});
+			});
+		} else if (result.error) {
+			this.error = result.error;
+		}
+	}
+
+	columnConfig = [
+		{
+			label: "Name",
+			fieldName: "name",
+			type: "text"
+		},
+		{
+			label: "Email",
+			fieldName: "email",
+			type: "email"
+		},
+		{
+			label: "Phone",
+			fieldName: "phone",
+			type: "phone"
+		}
+	];
+}
