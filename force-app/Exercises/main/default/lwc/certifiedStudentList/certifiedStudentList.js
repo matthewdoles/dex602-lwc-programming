95fa12688739815b1af0fa8677959e5931a7a15d
Matthew Doles <matteddied@gmail.com>
2019-10-24 11:15:20 -0400

    Exercise 5-4 Completed

--- force-app/Exercises/main/default/lwc/certifiedStudentList/certifiedStudentList.js
+++ force-app/Exercises/main/default/lwc/certifiedStudentList/certifiedStudentList.js
@@ -2,6 +2,7 @@ import { LightningElement, api, track, wire } from "lwc";
 import getCertifiedStudents from "@salesforce/apex/CertifiedStudentList.getCertifiedStudents";
 import deleteStudentCertification from "@salesforce/apex/CertifiedStudentList.deleteStudentCertification";
 import { refreshApex } from "@salesforce/apex";
+import utils from "c/utils";
 export default class CertifiedStudentList extends LightningElement {
 	@api certificationId = 0;
 	@api certificationName = "";
@@ -73,8 +74,10 @@ export default class CertifiedStudentList extends LightningElement {
 		const btnClicked = event.target.getAttribute("data-btn-id");
 		switch (btnClicked) {
 			case "btnEmail":
+				this.notAvailable();
 				break;
 			case "btnSendCert":
+				this.notAvailable();
 				break;
 			case "btnDelete":
 				this.onDelete();
@@ -93,5 +96,9 @@ export default class CertifiedStudentList extends LightningElement {
 			.catch(error => {
 				this.error = error;
 			});
+	}
+
+	notAvailable() {
+		utils.showModal(this, "Not Available", "This feature is currently unavailable");
 	}
 }
