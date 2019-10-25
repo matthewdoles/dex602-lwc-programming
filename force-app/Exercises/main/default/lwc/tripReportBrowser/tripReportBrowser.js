9e1fd678aab890157fb36a7b8b745a83e26af501
Matthew Doles <matteddied@gmail.com>
2019-10-25 09:53:32 -0400

    Exercise 6-4 Edit Record

--- force-app/Exercises/main/default/lwc/tripReportBrowser/tripReportBrowser.js
+++ force-app/Exercises/main/default/lwc/tripReportBrowser/tripReportBrowser.js
@@ -70,4 +70,12 @@ export default class TripReportBrowser extends LightningElement {
 	onBtnNewClick() {
 		this.changeTripReportMode("add");
 	}
+
+	handleRowClick(event) {
+		this.selectedRecordId = event.detail.pk;
+	}
+
+	handleRowDblClick() {
+		this.changeTripReportMode('edit');
+	}
 }
